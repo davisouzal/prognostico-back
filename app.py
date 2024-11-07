4c9b12832cf62f8f4d0327457f7db46ee4b7fffa
Davi Souza <dsl@ic.ufal.br>
2024-11-07 07:33:43 -0300

    :sparkles: feat: cors config

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from datetime import datetime
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from prognosis import calcular_pontuacao, obter_prognostico, obter_prognostico_humanizado
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
 db = SQLAlchemy(app)
